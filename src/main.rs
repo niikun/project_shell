308cff19af141cb7e78d7ff51a6b2d7215cd75bf
niikun <niikun0209@gmail.com>
2026-05-30 12:53:54 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() {
             "type" => {
                 let arg_second = args.split_whitespace().next().unwrap_or("");
                 match arg_second{
-                    "exit" | "echo" => println!("{} is a shell builtin",arg_second),
+                    "exit" | "echo" | "type"=> println!("{} is a shell builtin",arg_second),
                     _ =>  println!("{}: command not found",arg_second)
                 }
 
