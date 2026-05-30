6f520e95c34d60f23517ca23062fc9a7008800b0
niikun <niikun0209@gmail.com>
2026-05-30 14:41:37 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -34,10 +34,10 @@ fn main() {
                     let found =  path_env.split(":") .any(|path|{
                         let file_path = format!("{}/{}", path, arg_second);
                         if std::path::Path::new(&file_path).exists() {
-                            let meta = std::fs::metadata(&file_path).unwrap();
-                            if meta.permissions().mode() & 0o111 != 0 {
-                                println!("{} is {}", arg_second, &file_path);
-                            }
+                            // let meta = std::fs::metadata(&file_path).unwrap();
+                            // if meta.permissions().mode() & 0o111 != 0 {
+                            //     println!("{} is {}", arg_second, &file_path);
+                            // }
                             true
                         } else {
                             false
