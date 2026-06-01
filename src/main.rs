8555a48307a30b4944c851ae8a20cbd4f117a25a
niikun <niikun0209@gmail.com>
2026-06-02 06:14:11 +0900

    fix:backslash in single quote

--- src/main.rs
+++ src/main.rs
@@ -77,8 +77,7 @@ fn parse_args(input: &str) -> Vec<String> {
     for ch in input.chars() {
         if !is_backslashed{
             match ch {
-                // 課題！！
-                '\\' => is_backslashed = !is_backslashed,
+                '\\' if !in_single_quote => is_backslashed = !is_backslashed,
                 '\'' if !in_double_quote => in_single_quote = !in_single_quote,
                 '\"' => in_double_quote = !in_double_quote,
                 ' ' | '\t' if !in_single_quote && !in_double_quote => {
